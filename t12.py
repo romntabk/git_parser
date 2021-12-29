308d7ad4e77aa53f83e3d51f44c570851ae4d289
romchik <romntabk@mail.ru>
2021-12-29 05:04:44 +0300

    final version

---
+++ t12.py
@@ -0,0 +1,113 @@
+from github import Github
+import requests
+import os
+import regex
+import time
+import logging
+
+
+def timer1(f):
+    def wrapper(*args):
+        start = time.time()
+        res = f(*args)
+        print(f'Exectuion time - {time.time()-start}s')
+        return res 
+    return wrapper
+
+
+class git_parser:
+    def __init__(self,token = '',nickname='username'):
+        '''
+        Keyword arguments:
+            token = '' -- github token
+            nickname = 'username' -- nickname for authorized requests
+
+            authorization is needed to increase 
+            the limit of requests to the github api
+        '''
+        self.data = []
+        self.auth=''
+        self.uncheched_branches={}
+        if token == '':
+            self.git = Github()
+        else:      
+            self.auth = (nickname, token)
+            self.git = Github (login_or_token=token)
+
+
+    def get_statistic(self, name):
+        '''
+        Keyword arguments:
+            name -- user name on github
+        
+        list that contains tuples of the form 
+        (repository name, dictionary of year - number of commits pairs)
+        return    [repository name, {year_i : number of commits in year_i, ...}]
+        returntype[       str     , {  str  :             int            , ...}]
+        '''
+        user = self.git.get_user(name)
+        arr=[]
+        self.user_name=name
+        for rep in user.get_repos():
+            branches = requests.get(rep.branches_url[:-9],auth=self.auth)
+            rep_data=(rep.name,self._parse_branches(branches.json(),rep.name))
+            self.data.append(rep_data)
+        return self.data#, f'LOG: uncheched_branches = {self.uncheched_branches}'
+
+
+    @timer1
+    def _parse_branches(self,branches,rep):
+        print(f'repository: {rep}',end=' ')
+        dict_ = {}#{'2019':12}
+        for branche in branches:
+            try:
+                print(f'branch:', branche['name'])
+                last_sha = branche['commit']['sha'] 
+                url_first_commit =  f'https://api.github.com/repos/{self.user_name}/{rep}/commits'
+                first_commit = requests.get(url_first_commit, auth=self.auth)
+                link =first_commit.headers.get('Link')
+                if link:
+                     page_url = link.split(',')[1].split(';')[0].split('<')[1].split('>')[0]
+                     # page_url2 = link.split(';')[0][1:-1] DOESNT WORK :C WHY :(
+                     # first_commit2 =requests.get(page_url2,auth=self.auth).json() 
+                     first_commit = requests.get(page_url,auth=self.auth)
+                first_commit=first_commit.json()
+                first_sha=first_commit[-1]['sha']
+                url_compare_sha = f'https://api.github.com/repos/{self.user_name}/{rep}/compare/{first_sha}...{last_sha}'
+                compare_response = requests.get(url_compare_sha, auth=self.auth).json()
+                self._parse_branche(compare_response,dict_)
+            except:
+                if rep in self.uncheched_branches:
+                    self.uncheched_branches[rep]+=1
+                else:
+                    self.uncheched_branches[rep]=1
+        return dict_
+    
+
+    def _parse_branche(self,compare,dict_):
+        bonus_create=1
+        for i in compare['commits']:
+            if type(i['author']) is type(dict) :
+                nick =i['author']['login']
+            else:
+                nick = i['commit']['author']['name']
+            if nick == self.user_name:
+                if i['commit']['author']['date'][:4] in dict_:
+                    dict_[i['commit']['author']['date'][:4]] += 1+bonus_create
+                else:
+                    dict_[i['commit']['author']['date'][:4]] = 1+bonus_create
+                bonus_create=0
+    
+
+
+
+def main():
+    token = ''
+    nickname = 'romntabk'
+    git_p = git_parser(token)
+    data= git_p.get_statistic('romntabk')
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
---
+++ t13_draw.py
@@ -0,0 +1,45 @@
+# import cufflinks as cf
+import plotly
+import plotly.graph_objs as go
+import numpy as np
+from plotly.subplots import make_subplots
+import pandas as pd
+from t12 import git_parser
+
+def pie_data(df,series,year='-1'):
+	if year=='-1':
+		z = [str(i) for i in df['Data'] if len(i) !=0]
+	else:
+		z = [str(i['2021'])  for i in df['Data'] if len(i)!=0 and year in i ]
+	
+	c=[df['Rep'][i] for i in range(len(series)) if series[i]!=0]
+	series = list(filter(lambda x: x!=0,series))
+	pull = [0]*len(series)
+	pull[list(series).index(max(series))] = 0.1
+	return {'name': 'repository', 'values':series,'hovertext':z,'labels':c, 'pull':pull, 'hole':0.3}
+def draw(data):
+	# data =[('basic-data-collection-app', {'2020': 9}), ('basic-endpoint', {}), ('basic-vk-oauth-web-app', {'2020': 1}), ('basic_yandex_news_parser', {'2020': 2}), ('file-metadata-cpp', {}), ('Mob.dev', {}), ('OS_labs', {'2020': 10}), ('pydes', {}), ('python-exercises', {}), ('test-repo', {}), ('tgk_radio', {'2020': 102}), ('three.js', {}), ('timer', {'2019': 4}), ('triglinkiEvents', {'2021': 1}), ('visual_calc', {'2019': 1}), ('vk_3glinki_bot', {}), ('vk_bot_music_on_pc', {'2020': 5}), ('vk_tgk_bot_2', {'2020': 4}), ('wiki_parser', {'2019': 14}), ('youtube-dl', {})]
+	
+	df = pd.DataFrame(data , columns = ['Rep', 'Data'])
+	series1=[sum(i.values()) if len(i)!=0 else 0 for i in df['Data'] ]
+	series2=[i['2021'] if len(i)!=0 and '2021' in i.keys() else 0 for i in df['Data']]
+	pie1=pie_data(df,series1)
+	pie2=pie_data(df,series2,year='2021')
+	fig = make_subplots(rows=1, cols=2, specs=[[{'type':'domain'}, {'type':'domain'}]])
+	fig.add_trace(go.Pie(pie1),1,1)
+	fig.add_trace(go.Pie(pie2),1,2)
+	fig.update_layout(
+	    title_text="GitHub commits",
+	    # Add annotations in the center of the donut pies.
+	    annotations=[dict(text='total', x=0.215, y=0.5, font_size=20, showarrow=False),
+	                 dict(text='last_year', x=0.82, y=0.5, font_size=20, showarrow=False)])
+	plotly.offline.plot(fig)
+def main():
+	token = ''
+	login = 'mist-leet'
+	git_p = git_parser(token)
+	data= git_p.get_statistic(login)
+	draw(data)
+
+if __name__ =='__main__':
+	main()
